d5d2fa6db30c6514de30d3f47c55b1668fbf1548
ArRosid <ahmadrosid30121997@gmail.com>
2020-01-15 10:55:36 +0700

    setup database for shop app

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ['name', 'slug']
+	prepopulated_fields = {'slug': ('name',)}
+
+class ProductAdmin(admin.ModelAdmin):
+	list_display = ['name', 'slug', 'price', 'stock', 'available', 'created_at', 'updated_at']
+	list_filter = ['available', 'created_at', 'updated_at']
+	list_editable = ['price', 'stock', 'available']
+	prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(models.Product, ProductAdmin)
+admin.site.register(models.Category, CategoryAdmin)
