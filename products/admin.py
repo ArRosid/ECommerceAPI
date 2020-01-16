d7239c47b0c22d4ead08540fd5f198455d7c20e2
ArRosid <ahmadrosid30121997@gmail.com>
2020-01-16 14:50:23 +0700

    Custom manager & queryset for product models

--- products/admin.py
+++ products/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["title", "active", "featured", "price"]
+    list_editable = ["active", "featured", "price"]
+    list_filter = ["title","price"]
+
+admin.site.register(models.Product, ProductAdmin)
